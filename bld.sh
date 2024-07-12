694dcfb2a8549f66758015bcc50f3262f0e179cb
Steve Ford <sford@geeky-boy.com>
2024-07-12 12:59:55 -0400

    bugfix

--- bld.sh
+++ bld.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # bld.sh - not much to build; just doc TOC
 
-# Update doc table of contents (see https://github.com/fordsfords/mdtoc).
-if which mdtoc.pl >/dev/null; then mdtoc.pl -b "" README.md;
-elif [ -x ../mdtoc/mdtoc.pl ]; then ../mdtoc/mdtoc.pl -b "" README.md;
-else echo "FYI: mdtoc.pl not found; see https://github.com/fordsfords/mdtoc"
-fi
+for F in *.md; do :
+  if egrep "<!-- mdtoc-start -->" $F >/dev/null; then :
+    # Update doc table of contents (see https://github.com/fordsfords/mdtoc).
+    if which mdtoc.pl >/dev/null 2>&1; then LANG=C mdtoc.pl -b "" $F;
+    elif [ -x ../mdtoc/mdtoc.pl ]; then LANG=C ../mdtoc/mdtoc.pl -b "" $F;
+    else echo "FYI: mdtoc.pl not found; Skipping doc build"; echo ""; fi
+  fi
+done
