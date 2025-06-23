ccc772dd85df71a5b8860ad49f5c68af19b114f3
Steve Ford <sford@geeky-boy.com>
2025-06-23 15:11:08 -0400

    update

--- pinger.sh
+++ pinger.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # monnet.sh
 
+IP="$1"
+if [ -z "$IP" ]; then :
+  echo "pinger: Using 204.148.47.162 (verizon.com.customer.alter.net)"
+  IP=204.148.47.162
+fi
+
 while true; do :
-  ping -W 1 -n -c 1 74.6.143.26 >/dev/null
+  ping -W 1 -n -c 1 $IP >/dev/null
   if [ $? -ne 0 ]; then echo "Gap `date`"; fi
 done
